35fa80eeb414659785ddfcaf2ec47c3a93c1ca5c
Roey Kimhi <roey.kimhi@abra-it.com>
2026-01-15 19:21:44 +0200

    Adde API for creating and updating a comment

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const commentController = require("../controller/commentController.js");
 
 router.get("/comments/:postId", commentController.getCommentsByPostId);
+router.post("/comments/create", commentController.createComment);
+router.put("/comments/update", commentController.updateComment);
 
 module.exports = router;
