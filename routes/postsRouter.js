bd0f15a84cd240415609b5ad21495611c16ea729
AmitMichael12 <amit.michael100@gmail.com>
2026-01-17 17:12:28 +0200

    fix request.rest

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const postController = require("../controller/postsController.js");
 
 router.get("/posts", postController.getAllPosts);
-router.get("/posts/:id", postController.getPost);
 router.get("/posts/uploader", postController.getPostsByUploaderId);
+router.get("/posts/:id", postController.getPost);
 router.post("/posts/create", postController.createPost);
-router.put("/posts/update/:id", postController.updatePost)
-router.delete("/posts/delete", postController.deletePost)
+router.put("/posts/update/:id", postController.updatePost);
+router.delete("/posts/delete", postController.deletePost);
 
 module.exports = router;
